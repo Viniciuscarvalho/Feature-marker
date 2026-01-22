e8c2e8bc769109cf6d8cfb1caf7250146a634db4
Vinicius Carvalho <vinicius.carvalho@poatek.com>
2026-01-22 11:42:33 -0300

    Fix size of git and path

--- video-explainer/src/compositions/MainVideo.tsx
+++ video-explainer/src/compositions/MainVideo.tsx
@@ -8,28 +8,28 @@ import {OutroScene} from './OutroScene';
 export const MainVideo: React.FC = () => {
 	return (
 		<AbsoluteFill style={{backgroundColor: '#0a0a0a'}}>
-			{/* Scene 1: Intro (0-10s) */}
-			<Sequence from={0} durationInFrames={300}>
+			{/* Scene 1: Intro (0-6.6s) */}
+			<Sequence from={0} durationInFrames={200}>
 				<IntroScene />
 			</Sequence>
 
-			{/* Scene 2: Basic Command (10-20s) */}
-			<Sequence from={300} durationInFrames={300}>
+			{/* Scene 2: Basic Command (6.6-13.3s) */}
+			<Sequence from={200} durationInFrames={200}>
 				<BasicCommandScene />
 			</Sequence>
 
-			{/* Scene 3: Interactive Panel (20-40s) */}
-			<Sequence from={600} durationInFrames={600}>
+			{/* Scene 3: Interactive Panel (13.3-26.6s) */}
+			<Sequence from={400} durationInFrames={400}>
 				<InteractivePanelScene />
 			</Sequence>
 
-			{/* Scene 4: Workflow Execution (40-50s) */}
-			<Sequence from={1200} durationInFrames={300}>
+			{/* Scene 4: Workflow Execution (26.6-33.3s) */}
+			<Sequence from={800} durationInFrames={200}>
 				<WorkflowScene />
 			</Sequence>
 
-			{/* Scene 5: Outro (50-60s) */}
-			<Sequence from={1500} durationInFrames={300}>
+			{/* Scene 5: Outro (33.3-40s) */}
+			<Sequence from={1000} durationInFrames={200}>
 				<OutroScene />
 			</Sequence>
 		</AbsoluteFill>
